bbbf68597bfec345330b515e088eef6bbd8530ce
Christian Ipanaque <chris.ipanaque@gmail.com>
2019-08-21 11:08:43 -0700

    connected to backend api

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 // import Navigation from '../Navigation'
 
 const Home = () => {
+  const [welcomeMessage, setWelcomeMessage] = useState('');
+
+  useEffect(() => {
+    axios
+      .get('http://mongo3.letsmovehomie.com:3001/')
+      .then((response) => {
+        setWelcomeMessage(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
-    <>
+    <div>
       <h1>Home</h1>
-    </>
+      <h2>Welcome Message:</h2>
+      <p>{welcomeMessage}</p>
+    </div>
   );
 };
 
