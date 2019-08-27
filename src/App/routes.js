c3c049490124286384b5fb5080748eba1dc399e9
Ramses Bermudez <ramsesbermudez@Ramsess-MacBook-Pro.local>
2019-08-27 15:58:03 -0400

    Login and register components created and styled

--- src/App/routes.js
+++ src/App/routes.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
 import Home from '../Home';
+import Login from '../Authorization/login';
+import Register from '../Authorization/register';
 
 const Routes = () => {
   return (
     <>
       <Route exact path='/' component={Home} />
-      {/* <Route path='/templ' component={Template} /> */}
-      {/* <Route path='/multi' component={MultiStepForm} /> */}
+      <Route path='/register' component={Register} />
+      <Route path='/login' component={Login} />
       {/* <Route path='/'  component={} />  */}
       {/* <Route path='/'  component={} />  */}
       {/* <Route path='/'  component={} />  */}
