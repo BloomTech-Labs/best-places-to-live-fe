caf3dd6ca5928ac5ea434ba4210d9d7225e9772f
Carlos Santana <carlossantana987@outlook.com>
2019-08-29 19:15:42 -0400

    refractor login and register page

--- src/Authorization/login.js
+++ src/Authorization/login.js
@@ -1,13 +1,20 @@
 import React, {useState} from 'react';
 import axios from 'axios';
-import { FormContainer } from './styled';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import useStyles from './styled';
+
 
 
 const Login = () => {
-
 const [input , setInput] = useState({
-  firstName: '',
-  lastName: '',
   email: '',
   password: ''
 });
@@ -22,7 +29,6 @@ const handleChange = (event) => {
 
 const handleSubmit = (event, input) => {
   event.preventDefault();
-
   axios
     .post('http://167.71.246.202:3001/users/login', input)
     .then((response) => {
@@ -32,33 +38,70 @@ const handleSubmit = (event, input) => {
       console.log(err);
     });
 };
-  return (
-    <label htmlFor='Log-in'>
-      <FormContainer>
-      <form onSubmit={handleSubmit}>
-        <h1>Log-In</h1>
-        <input
-        value={input.email}
-        id='email'
-        type='email' 
-        placeholder='Email'
-        onChange={handleChange}
-        />
-        <br/>
-        <input
-        value={input.password}
-        id='password'
-        type='password' 
-        placeholder='Password'
-        onChange={handleChange}
-        />
-        <br/>
-        <button type='submit'>Login</button>
-      </form>
-      </FormContainer>
-    </label>
+const classes = useStyles();
 
+return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">Sign in</Typography>
+        <form className={classes.form} noValidate value={{handleSubmit}}>
+          <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                name="email"
+                label="Email Address"
+                type='email'
+                autoComplete="email"
+                value={input.email}
+                onChange={handleChange}
+                autoFocus/>
+          <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="password"
+                name="password"
+                label="Password"
+                type="password"
+                autoComplete="current-password"
+                value={input.password}
+                onChange={handleChange}/>
+
+            {/*remember me button when the time is needed to integrate it*/}
+            {/*<FormControlLabel*/}
+            {/*    control={<Checkbox value="remember" color="primary" />}*/}
+            {/*    label="Remember me"*/}
+            {/*/>*/}
+
+
+            <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}>Sign In</Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">Forgot password?</Link>
+              </Grid>
+              <Grid item>
+                <Link href="/register" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
+        </div>
+      </Container>
   );
-};
+}
 
 export default Login;
--- src/Authorization/register.js
+++ src/Authorization/register.js
@@ -1,10 +1,18 @@
 import React, {useState} from 'react';
 import axios from 'axios';
-import { FormContainer } from './styled';
+import useStyles from "./styled";
+import Avatar from "@material-ui/core/Avatar";
+import LockOutlinedIcon from "@material-ui/core/SvgIcon/SvgIcon";
+import Typography from "@material-ui/core/Typography";
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+import {TextField} from "@material-ui/core";
+import Button from "@material-ui/core/Button";
+import Link from "@material-ui/core/Link";
 
 
 const Register = () => {
-  const [input , setInput] = useState({
+  const [input, setInput] = useState({
     firstName: '',
     lastName: '',
     email: '',
@@ -13,75 +21,98 @@ const Register = () => {
 
 
   const handleChange = (event) => {
-  event.persist();
-  setInput((input) => ({
-    ...input,
-    [event.target.id]: event.target.value,
-  }));
-};
+    event.persist();
+    setInput((input) => ({
+      ...input,
+      [event.target.id]: event.target.value,
+    }));
+  };
 
   const handleSubmit = (event, input) => {
     event.preventDefault();
 
     axios
-      .post('http://demo0969329.mockable.io/register', input)
-      .then((response) => {
-        console.log(input)
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+        .post('http://demo0969329.mockable.io/register', input)
+        .then((response) => {
+          console.log(input)
+        })
+        .catch((err) => {
+          console.log(err);
+        });
   };
-  return (
-    <label htmlFor='Register'>
-    <FormContainer>
-      <form onSubmit={handleSubmit}>
-        <h1>Sign Up</h1>
-        <input
-        value={input.firstName}
-        id='firstName'
-        type='text' 
-        placeholder='First Name'
-        onChange={handleChange}
-        />
-        <br/>
-        <input 
-        value={input.lastName}
-        id='lastName'
-        type='text' 
-        placeholder='Last Name'
-        onChange={handleChange}
-        />
-        <br/>        
-        <input
-        value={input.email}
-        id='email'
-        type='email' 
-        placeholder='Email'
-        onChange={handleChange}
-        />
-        <br/>        
-        <input
-        value={input.password}
-        id='password'
-        type='password' 
-        placeholder='Password'
-        onChange={handleChange}
-        />
-        <br/>
-        <input
-        id='password'
-        type='password' 
-        placeholder='Confirm password'
-        onChange={handleChange}
-        />
-        <br/>
-        <button type='submit'>Register</button>
-      </form>
-    </FormContainer>
-    </label>
 
-  );
-};
+  const classes = useStyles();
+
+  return (
+      <Container component="main" maxWidth="xs">
+        {/*<CssBaseLine />*/}
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon/>
+          </Avatar>
+          <Typography component="h1" variant="h5">Sign Up</Typography>
+          <form className={classes.form} noValidate value={{handleSubmit}}>
+              <TextField
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="firstName"
+                  name="FirstName"
+                  label="First Name"
+                  type="text"
+                  value={input.firstName}
+                  onChange={handleChange}
+                  autoFocus
+              />
+            <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="lastName"
+                name="lastName"
+                label="Last Name"
+                type="text"
+                value={input.lastName}
+                onChange={handleChange}
+                autoFocus
+            />
+            <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                name="email"
+                label="Email"
+                type="email"
+                value={input.email}
+                onChange={handleChange}
+                autoFocus
+            />
+            <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="password"
+                name="password"
+                label="Password"
+                type="password"
+                value={input.password}
+                onChange={handleChange}
+                autoFocus
+            />
+            <Button type='submit'> Register </Button>
+            <Link href="/register" variant="body2">
+              {"Already have an account? Login In"}
+            </Link>
+          </form>
+        </div>
+      </Container>
+
+  )
+}
 
 export default Register;
