e85b20f41ded69b0fe7b0fb2ac3f6f051b9a2910
Ramses Bermudez <ramsesbermudez@Ramsess-MacBook-Pro.local>
2019-09-03 12:29:54 -0400

    formatted file with prettier

--- src/Navigation/index.js
+++ src/Navigation/index.js
@@ -1,47 +1,54 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import React from "react";
+import { Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
 
 const useStyles = makeStyles(theme => ({
-    root: {
-        flexGrow: 3,
-    },
-    menuButton: {
-        marginRight: theme.spacing(2),
-    },
-
+  root: {
+    flexGrow: 3
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  }
 }));
 
 export default function Navigation() {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <div className={classes.root}>
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                        <MenuIcon />
-                    </IconButton>
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
 
-                    <Link to="/" style={{textDecoration:'none',color:'white'}}>
-                        <Button color="inherit">Home</Button>
-                    </Link>
+          <Link to="/" style={{ textDecoration: "none", color: "white" }}>
+            <Button color="inherit">Home</Button>
+          </Link>
 
-                    <Link to="/topten" style={{textDecoration:'none',color:'white'}}>
-                        <Button color="inherit">Top10</Button>
-                    </Link>
+          <Link to="/topten" style={{ textDecoration: "none", color: "white" }}>
+            <Button color="inherit">Top10</Button>
+          </Link>
 
-                    <Link to="/Login" style={{textDecoration:'none',color:'white'}}>
-                        <Button color="inherit">Login</Button>
-                    </Link>
+          <Link to="/Login" style={{ textDecoration: "none", color: "white" }}>
+            <Button color="inherit">Login</Button>
+          </Link>
 
-                </Toolbar>
-            </AppBar>
-        </div>
-    );
+          <Link to="/Register" style={{ textDecoration: "none", color: "white" }}>
+            <Button color="inherit">Login</Button>
+          </Link>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
