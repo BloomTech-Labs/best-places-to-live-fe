b391e2b944fe4b5ad25715d315726c19e7264f62
harjoth <harjoth.khara@gmail.com>
2019-09-09 16:27:54 -0700

    simplified and removed repo info from profile view

--- src/UserList/components/users/User.js
+++ src/UserList/components/users/User.js
@@ -1,21 +1,17 @@
 import React, { Fragment, Component } from 'react';
 import Spinner from '../layout/Spinner';
-import Repos from '../repos/Repos';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 export class User extends Component {
   componentDidMount() {
     this.props.getUser(this.props.match.params.login);
-    this.props.getUserRepos(this.props.match.params.login);
   }
 
   static propTypes = {
     loading: PropTypes.bool,
     user: PropTypes.object.isRequired,
-    repos: PropTypes.array.isRequired,
     getUser: PropTypes.func.isRequired,
-    getUserRepos: PropTypes.func.isRequired,
   }
 
   render() {
@@ -30,8 +26,6 @@ export class User extends Component {
       html_url,
       followers,
       following,
-      public_repos,
-      public_gists,
       hireable
     } = this.props.user;
 
@@ -69,7 +63,7 @@ export class User extends Component {
               </Fragment>
             )}
             <a href={html_url} className='btn btn-dark my-1'>
-              Visit Github Profile
+              Visit Profile
             </a>
             <ul>
               <li>
@@ -89,7 +83,6 @@ export class User extends Component {
                   <strong>Website: </strong> {blog}
                   </Fragment> }
               </li>
-
             </ul>
           </div>
         </div>
@@ -97,11 +90,8 @@ export class User extends Component {
         <div className="card text-center">
           <div className="badge badge-primary">Followers: {followers} </div>
           <div className="badge badge-success">Following: {following} </div>
-          <div className="badge badge-light">Public Repos: {public_repos} </div>
-          <div className="badge badge-dark">Public Gists: {public_gists} </div>
         </div>
 
-        <Repos repos={repos} />
       </Fragment>
     );
   }
