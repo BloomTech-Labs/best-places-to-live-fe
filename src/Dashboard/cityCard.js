c6cbdc3e1828e7f796ab40f521f62255ff21e325
Ramses Bermudez <ramsesbermudez@Ramsess-MacBook-Pro.local>
2019-09-12 18:13:51 -0400

    creted all necessary component placeholders for the dashboard mvp

---
+++ src/Dashboard/cityCard.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { CityCard, CardDetail} from "./styled"
+
+
+const CityCards = props => {
+  return (
+    <CityCard>
+        <img
+          component="img"
+          alt="City Image"
+          title="City Image"
+          src={props.card.photo}/>
+          <CardDetail>
+
+              <p>{props.card.name}</p>
+
+              <i className="fas fa-money-check-alt"></i>
+              <p>{props.card.cost_of_living}</p>
+
+              <i className="fas fa-car"/>
+              <p>{props.card.avg_commute_time}</p>
+
+        </CardDetail>
+    </CityCard>
+  );
+};
+
+export default CityCards;
--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -1,28 +1,51 @@
-import React from 'react';
-import { Link } from "react-router-dom";
-import Navigation from '../Navigation';
-import { MenuBox, UserInfoBox, ContentBox } from "./styled";
-import SavedCities from './savedCities';
+import React from "react";
+import { Link, Route } from "react-router-dom";
+import Navigation from "../Navigation";
+import { MenuBox, UserInfoBox, ContentBox, ComponentBox} from "./styled";
+import SavedCities from "./savedCities";
+import Profile from "./profileSettings";
+import Specialist from "./citySpecialist";
 
-
-
-const DashboardMenu = () => {
+const DashboardMenu = ({ match }) => {
   return (
     <div>
       <Navigation />
       <UserInfoBox></UserInfoBox>
       <ContentBox>
         <MenuBox>
-          <Link to='/Dashboard/Profile'>Profile</Link>
-          <Link to='/Dashboard/SavedCities'>Saved Cities</Link>
-          <Link to='/Dashboard/Specialist'>Become a Specialist</Link>
-          <Link to='/Dashboard/Messages'>Messages</Link>
+          <Link to={`${match.url}/Profile`}>Profile</Link>
+          <Link to={`${match.url}/SavedCities`}>Saved Cities</Link>
+          <Link to={`${match.url}/Specialist`}>Become a Specialist</Link>
+          <Link to={`${match.url}/Messages`}>Messages</Link>
         </MenuBox>
-        {/* <Profile/>
-        <SavedCities/> */}
+        <Route path={`${match.path}/:optionId`} component={Option} />
+        <Route
+          exact
+          path={match.path}
+          render={() => <h3>Please select an option.</h3>}
+        />
       </ContentBox>
     </div>
-  )
+  );
+};
+
+function Option({ match }) {
+  let comp;
+
+  switch (match.params.optionId) {
+    case "Profile":
+      comp = <Profile />;
+      break;
+    case "SavedCities":
+      comp = <SavedCities />;
+      break;
+    case "Specialist":
+      comp = <Specialist />;
+      break;
+    default:
+      comp = <div>Choose an option</div>;
+  }
+  return <ComponentBox>{comp}</ComponentBox>;
 }
 
 export default DashboardMenu;
