eb178d8fff9317e0f1f4c49306f565584a888057
tactnician <tactnician@gmail.com>
2019-09-16 09:39:02 -0400

    revert modification to Home

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,10 +1,8 @@
+import React from "react";
 import Navigation from "../Navigation";
 import HomeSearchBar from "../HomeSearchBar";
 import TopTenGrid from "../TopTen";
-import React from 'react';
-import Navigation from '../Navigation';
-import HomeSearchBar from '../HomeSearchBar';
-import TopTenGrid from '../TopTen';
+import GoogleMap from "../GoogleMap";
 
 const Home = () => {
   return (
