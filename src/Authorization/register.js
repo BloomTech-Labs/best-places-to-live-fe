4f2d5be8fa347f118ec97a09cb671bf7ea6942df
chris.j.carter <chris.j.carter91@gmail.com>
2019-09-18 16:41:29 -0600

    Fixed cluster you know what

--- src/Authorization/register.js
+++ src/Authorization/register.js
@@ -1,17 +1,9 @@
 import React, {useState} from 'react';
-import Navigation from '../Navigation'
-import Step1 from './step1'
-import Step2 from './step2'
-import Step3 from './step3'
-// import axios from 'axios';
-// import { Link } from "react-router-dom";
-// import {LoginImg, LoginContainer,LoginFormParent,LoginForm,RegisterBottomLinks, SignInButton} from "./styled";
-// import Icon from "../images/LMHiconcopy.png";
-
-
+import axios from 'axios';
+import { Link } from "react-router-dom";
+import {LoginImg, LoginContainer,LoginFormParent,LoginForm,RegisterBottomLinks, SignInButton} from "./styled";
+import Icon from "../images/LMHiconcopy.png";
 const Register = () => {
-
-
   const [input, setInput] = useState({
     // Commenting out these key:value pairs until the back end is refractored to receive them
     // firstName: '',
@@ -21,8 +13,6 @@ const Register = () => {
     password: '',
     password2: '',
   });
-
-
   const handleChange = (event) => {
     event.persist();
     setInput((input) => ({
@@ -30,7 +20,6 @@ const Register = () => {
       [event.target.id]: event.target.value,
     }));
   };
-
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log(input)
@@ -44,22 +33,16 @@ const Register = () => {
           console.log(error.response);
         });
   };
-
   return (
       <>
         <LoginImg />
-
         <LoginContainer>
-
           <LoginFormParent>
             <Link to="/">
               <img alt="Icon"src={Icon}/>
             </Link>
-
             <h1>Sign Up</h1>
-
             <LoginForm className='' onSubmit={handleSubmit}>
-
                    <input
                        id="name"
                        name="name"
@@ -69,7 +52,6 @@ const Register = () => {
                        value={input.name}
                        onChange={handleChange}
                    />
-
                    <input
                        id="email"
                        name="email"
@@ -79,7 +61,6 @@ const Register = () => {
                        value={input.email}
                        onChange={handleChange}
                    />
-
                    <input
                        id="password"
                        name="password"
@@ -89,7 +70,6 @@ const Register = () => {
                        value={input.password}
                        onChange={handleChange}
                    />
-
                    <input
                        id="password2"
                        name="password2"
@@ -99,7 +79,6 @@ const Register = () => {
                        value={input.password2}
                        onChange={handleChange}
                    />
-
                    <SignInButton>
                      Register
                    </SignInButton>
@@ -114,139 +93,4 @@ const Register = () => {
       </>
   )
 }
-
-    const [step, setStep] = useState(0)
-
-    //Shared Components
-    const NextBtn = () => {
-        return (
-            <button onClick = {() =>  {
-                step < 2 ? setStep(step + 1) : setStep(2)
-            }}>
-                Next
-            </button>
-        )
-    }
-
-    const BackBtn = () => {
-        return(
-            <button onClick = {() => {
-                step > 0 ? setStep( step - 1 ) : setStep(0) 
-            }} >
-                Back
-            </button>
-        )
-    }
-
-    switch(step) {
-        case 0: 
-            return(
-                <div className = "form">
-                    <Navigation  />
-                    <Step1
-                        // db = {localDb}
-                        className = 'step1'
-                        back = {BackBtn}
-                        next = {NextBtn}
-                    />
-                </div>
-            )
-        case 1:
-            return(
-                <div className = "form">
-                    <Navigation />
-                    <Step2 
-                        // db = {localDb}
-                        className = 'step2'
-                        back = {BackBtn}
-                        next = {NextBtn}
-                    />
-                </div>
-            )
-        case 2:
-            return(
-                <div className = "form">
-                    <Navigation />
-                    <Step3
-                        // db = {localDb}
-                        className = 'step3'
-                        back = {BackBtn}
-                        next = {NextBtn}
-                    />
-                </div>
-            )
-        default:
-            return setStep(0)
-    }
-}
-//   return (
-//       <>
-//         <LoginImg />
-
-//         <LoginContainer>
-
-//           <LoginFormParent>
-//             <Link to="/">
-//               <img src={Icon}/>
-//             </Link>
-
-//             <h1>Sign Up</h1>
-
-//             <LoginForm className='' onSubmit={handleSubmit}>
-
-//                    <input
-//                        id="name"
-//                        name="name"
-//                        label="name"
-//                        type="text"
-//                        placeholder="name"
-//                        value={input.name}
-//                        onChange={handleChange}
-//                    />
-
-//                    <input
-//                        id="email"
-//                        name="email"
-//                        label="Email"
-//                        type="email"
-//                        placeholder="email"
-//                        value={input.email}
-//                        onChange={handleChange}
-//                    />
-
-//                    <input
-//                        id="password"
-//                        name="password"
-//                        label="Password"
-//                        type="password"
-//                        placeholder="password"
-//                        value={input.password}
-//                        onChange={handleChange}
-//                    />
-
-//                    <input
-//                        id="password2"
-//                        name="password2"
-//                        label="Confirm password"
-//                        type="password"
-//                        placeholder="confirm password"
-//                        value={input.password2}
-//                        onChange={handleChange}
-//                    />
-
-//                    <SignInButton>
-//                      Register
-//                    </SignInButton>
-//             </LoginForm>
-//           </LoginFormParent>
-//           <RegisterBottomLinks>
-//           <Link to="/login">
-//             {"Already have an account? Login In"}
-//           </Link>
-//           </RegisterBottomLinks>
-//         </LoginContainer>
-//       </>
-//   )
-// }
-
-export default Register
+export default Register;
