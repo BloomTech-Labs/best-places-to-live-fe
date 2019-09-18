da6c2b563c6332062a8bf8b5f954464470353b10
harjoth <harjoth.khara@gmail.com>
2019-09-17 17:26:08 -0700

    added google analytics dependency, intialized in app.js

--- src/App/index.js
+++ src/App/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ga from 'react-ga';
 import { Router, withRouter } from 'react-router-dom';
 import Routes from './routes';
 import '../index.css';
@@ -6,6 +7,15 @@ import '../index.css';
 const createHistory = require('history').createBrowserHistory;
 const history = createHistory();
 const AppWithRouter = withRouter(Routes);
+
+// Should this be moved to env vars? E.g. ga.initialize(process.env.GA_ID)
+/* or, we could just run in production--if there is no benefit to getting analytics on staging
+      if (process.env.DB_ENV === 'production') {
+        ga.initialize('UA-143824465-1');
+      }
+    */
+ga.initialize('UA-148058893-1', { debug: true });
+ga.pageview('/');
 
 const App = () => {
   return (
