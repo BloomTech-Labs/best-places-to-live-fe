ad30d686b01c5f2fa705eab4d9e653c940398098
devin-bielejec <devin.bieleje@gmail.com>
2019-11-09 16:00:42 -0500

    Finished SignUp

---
+++ src/components/signUp.js
@@ -0,0 +1,48 @@
+import React from "react";
+import useForm from "react-hook-form";
+import { connect } from "net";
+
+export default function SignUp(props) {
+  const { register, handleSubmit, errors } = useForm();
+  const onSubmit = data => props.register(data);
+  console.log(errors);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input
+        type="text"
+        placeholder="First name"
+        name="First name"
+        ref={register({ required: true, maxLength: 80 })}
+      />
+      <input
+        type="text"
+        placeholder="Last name"
+        name="Last name"
+        ref={register({ required: true, maxLength: 100 })}
+      />
+      <input
+        type="text"
+        placeholder="Email"
+        name="Email"
+        ref={register({ required: true, pattern: /^\S+@\S+$/i })}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        name="Password"
+        ref={register({ required: true })}
+      />
+      <input
+        type="text"
+        placeholder="Location"
+        name="Location"
+        ref={register({ required: true, minLength: 2 })}
+      />
+
+      <input type="submit" />
+    </form>
+  );
+}
+
+// export default connect(null, {register})(SignUp);
