f04c8f4678bea50a7ab22329bbb9235028d3af2b
devin-bielejec <devin.bieleje@gmail.com>
2019-11-11 11:47:00 -0500

    Login and Signup action creators created
    Co-authored-by: Seth Lachman <lachmanseth17@gmail.com>

--- src/action/actionCreator.js
+++ src/action/actionCreator.js
@@ -1,4 +1,37 @@
-//something like this
-export function addTodo(text) {
-  return { type: ADD_TODO, text };
-}
+import { axiosWithAuth } from "../utils/axiosWithAuth.js";
+import {
+  LOGIN_INITIALIZE,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  SIGNUP_INITIALIZE,
+  SIGNUP_SUCCESS,
+  SIGNUP_FAIL
+} from "./index";
+
+export const login = credentials => dispatch => {
+  dispatch({ type: LOGIN_INITIALIZE });
+
+  axiosWithAuth()
+    .post(`/users/login`)
+    .then(res => {
+      localStorage.setItem("token", res.data.token);
+      localStorage.setItem("id", res.data.id);
+      dispatch({ type: LOGIN_SUCCESS, payload: res.data.id });
+    })
+    .catch(err => {
+      dispatch({ type: LOGIN_FAIL, payload: err });
+    });
+};
+
+export const register = userData => dispatch => {
+  dispatch({ type: SIGNUP_INITIALIZE });
+
+  axiosWithAuth
+    .post(`/users/register`)
+    .then(res => {
+      localStorage.setItem("token", res.data.token);
+      localStorage.setItem("id", res.data.id);
+      dispatch({ type: SIGNUP_SUCCESS, payload: res.data.id });
+    })
+    .catch(err => ({ type: SIGNUP_FAIL, payload: "Error: Please try again" }));
+};
